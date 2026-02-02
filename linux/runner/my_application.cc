3019ad0d5b6e03e90da7818f29baac47a02b3621
chetstriker <stover.jay@gmail.com>
2026-02-02 16:55:56 -0500

    linux fix

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -53,6 +53,9 @@ static void my_application_activate(GApplication* application) {
   }
 
   gtk_window_set_default_size(window, 1280, 720);
+  /* Ensure the window can be resized by the user and has decorations. */
+  gtk_window_set_resizable(window, TRUE);
+  gtk_window_set_decorated(window, TRUE);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(
